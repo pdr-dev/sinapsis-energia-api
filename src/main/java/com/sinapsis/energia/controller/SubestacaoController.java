bdb7de9527a2c1cb2d57cdec971da0aba00593c4
Pedro Henrique <contatophcs@gmail.com>
2020-10-09 23:56:22 -0300

    Finalizando controller para o sistema de crud para a entidade de subestação

--- src/main/java/com/sinapsis/energia/controller/SubestacaoController.java
+++ src/main/java/com/sinapsis/energia/controller/SubestacaoController.java
@@ -4,14 +4,17 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
+import javax.xml.ws.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -52,7 +55,27 @@ public class SubestacaoController {
 		if (subestacaoExistente.isPresent()) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe uma Subestação com esse código!");
 		}
-		
+
 		return subestacaoRepository.save(subestacao);
 	}
+
+	@PutMapping
+	public ResponseEntity<Subestacao> alterar(@PathVariable Long idSubestacao,
+			@Valid @RequestBody Subestacao subestacao) {
+		Optional<Subestacao> subestacaoExistente = subestacaoRepository.findById(idSubestacao);
+
+		if (!subestacaoExistente.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Não foi encontrado uma Subestação com essa ID!");
+		}
+
+		subestacao.setIdSubestacao(idSubestacao);
+		subestacaoRepository.save(subestacao);
+		return ResponseEntity.ok(subestacao);
+	}
+
+	@DeleteMapping
+	public void excluir(@PathVariable Long idSubestacao) {
+		subestacaoRepository.deleteById(idSubestacao);
+	}
+
 }
